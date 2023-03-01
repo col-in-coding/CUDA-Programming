8c96e5a8e8aa37e9a064eb8bacdb8335e145aa35
col-in-coding <hxl.colin@gmail.com>
2023-03-01 10:59:59 +0800

    monochrome GPU version

--- fundamental/Memory1D/monochrome.cu
+++ fundamental/Memory1D/monochrome.cu
@@ -2,6 +2,19 @@
 #include <opencv2/imgcodecs.hpp>
 #include <iostream>
 
+// Standard CUDA API functions
+#include <cuda_runtime_api.h>
+
+__global__ void monochrome(uchar* input_data, uchar* output_data, int size)
+{
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    if (index >= size) return;
+
+    output_data[index] = input_data[index * 3] * 0.3125f +
+                         input_data[index * 3 + 1] * 0.5f +
+                         input_data[index * 3 + 2] * 0.1875f;
+}
+
 int main()
 {
     std::string image_path = "../data/starry_night.png";
@@ -26,9 +39,29 @@ int main()
     }
     */
 
-    // 
+    // /*
+    // on GPU
+    {
+        uchar *input_data_d, *output_data_d;
+        // 这里不能使用 sizeof(img.data)
+        int inp_bytes = img.total() * img.elemSize();
+        int out_bytes = output_img.total() * output_img.elemSize();
+        cudaMalloc(&input_data_d, inp_bytes);
+        cudaMalloc(&output_data_d, out_bytes);
+        
+        if (img.isContinuous())
+        {
+            int pixel_count = img.rows * img.cols;
+            int block_size = 256;
+            int n_blocks = (pixel_count + block_size - 1) / block_size;
+            cudaMemcpy(input_data_d, img.data, inp_bytes, cudaMemcpyHostToDevice);
+            monochrome<<<n_blocks, block_size>>>(input_data_d, output_data_d, pixel_count);
+            cudaMemcpy(output_img.data, output_data_d, out_bytes, cudaMemcpyDeviceToHost);
+        }
+    }
 
     cv::imwrite("test.png", output_img);
+    // */
 
     return 0;
 }
